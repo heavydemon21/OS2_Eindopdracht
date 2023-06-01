bdb70def44e5bc446fc598b1529cfe2ce8f091b3
UnavailableDev <ggwildplay@gmail.com>
2023-06-01 12:22:36 +0200

    fixed folders

---
+++ src/Block.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cstdint>
+
+class Block
+{
+private:
+    int16_t data[2048];
+    int phase; // 0 = created, 1 = filled,  2 = bassed, 3 = bassed+trebled, 4 = written
+    int index;
+
+public:
+    Block() = default;
+    Block(int newIndex)
+    {
+        index = newIndex;
+        phase = 0;
+    }
+
+    void setData(int16_t* newData)
+    {
+        for (int i = 0; i < 2048; i++)
+        {
+            data[i] = newData[i];
+        }
+    }
+
+    void setPhase(int newPhase)
+    {
+        phase = newPhase;
+    }
+
+    int getPhase()
+    {
+        return phase;
+    }
+
+    int getIndex()
+    {
+        return index;
+    }
+};
---
+++ src/Source.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+
+#include <math.h>
+#include <cstdio>
+
+void bassCoefficients(int intensity, double* b0, double* b1, double* b2, double* a1, double* a2)
+{
+    double frequency = 330;
+    double qFactor = 0.5;
+    double gain = intensity;
+    double sampleRate = 44100;
+
+    double pi = 4.0 * atan(1);
+    double a = pow(10.0, gain / 40);
+    double w0 = 2 * pi * frequency / sampleRate;
+    double alpha = sin(w0) / (2.0 * qFactor);
+    double a0 = (a + 1) + (a - 1) * cos(w0) + 2.0 * sqrt(a) * alpha;
+
+    *a1 = -(-2.0 * ((a - 1) + (a + 1) * cos(w0))) / a0;
+    *a2 = -((a + 1) + (a - 1) * cos(w0) - 2.0 * sqrt(a) * alpha) / a0;
+
+    *b0 = (a * ((a + 1) - (a - 1) * cos(w0) + 2.0 * sqrt(a) * alpha)) / a0;
+    *b1 = (2 * a * ((a - 1) - (a + 1) * cos(w0))) / a0;
+    *b2 = (a * ((a + 1) - (a - 1) * cos(w0) - 2.0 * sqrt(a) * alpha)) / a0;
+}
+
+void trebleCoefficients(int intensity, double* b0, double* b1, double* b2, double* a1, double* a2)
+{
+    double frequency = 3300;
+    double qFactor = 0.5;
+    double gain = intensity;
+    double sampleRate = 44100;
+
+    double pi = 4.0 * atan(1);
+    double a = pow(10.0, gain / 40);
+    double w0 = 2 * pi * frequency / sampleRate;
+    double alpha = sin(w0) / (2.0 * qFactor);
+    double a0 = (a + 1) - (a - 1) * cos(w0) + 2.0 * sqrt(a) * alpha;
+
+    *a1 = -(2.0 * ((a - 1) - (a + 1) * cos(w0))) / a0;
+    *a2 = -((a + 1) - (a - 1) * cos(w0) - 2.0 * sqrt(a) * alpha) / a0;
+
+    *b0 = (a * ((a + 1) + (a - 1) * cos(w0) + 2.0 * sqrt(a) * alpha)) / a0;
+    *b1 = (-2.0 * a * ((a - 1) + (a + 1) * cos(w0))) / a0;
+    *b2 = (a * ((a + 1) + (a - 1) * cos(w0) - 2.0 * sqrt(a) * alpha)) / a0;
+}
+
+long fileSize(FILE* file) {
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    return fileSize;
+
+}
